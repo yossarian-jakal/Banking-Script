0a6b83c2858e3fbe3c4aa99f6340a945822aa848
yossarian-jakal <80456851+yossarian-jakal@users.noreply.github.com>
2024-08-26 22:36:14 +1200

    Add files via upload

---
+++ view ASB output.R
@@ -0,0 +1,42 @@
+library(dplyr)
+
+
+
+setwd("/home/jack/Documents/Life/ASB exports")
+
+spend <- read.csv('Export20240825181759.csv', skip = 6, header = TRUE)
+spend<- as.data.frame(spend)
+
+spend$Date <- as.Date(spend$Date)
+
+str(spend)
+
+Income <- spend %>% filter(Amount > 0)
+
+out <- spend %>% filter(Amount < 0)
+
+
+AP <- out %>% filter(Tran.Type == 'D/D')
+
+unique(out$Tran.Type)
+
+Monthly <- spend %>% 
+    select(Date, Amount, Payee) %>%
+      arrange(Date) %>%
+      mutate(Month_Year = substr(Date, 1,7)) %>%
+      group_by(Month_Year)
+
+May <- Monthly %>% 
+  filter(Month_Year =='2024-05')
+
+Jan <- sum(January$Amount)
+Feb <- sum(February$Amount)
+Mar <- sum(March$Amount)
+April <- sum(April$Amount)
+May <- sum(May$Amount)
+June <- sum(June$Amount)
+July <- sum(July$Amount)
+August <- sum(August$Amount)
+
+
+
